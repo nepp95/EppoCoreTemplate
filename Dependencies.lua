7b787928e398b57b6c52d2744ed17b72fc647561
Niels Eppenhof <niels@eppostudios.nl>
2024-07-19 13:21:38 +0200

    Removed Premake

--- Dependencies.lua
+++
@@ -1,8 +0,0 @@
--- Include directories
-IncludeDir = {}
-IncludeDir["glad"] = "%{wks.location}/EppoCore/EppoCore/Vendor/glad/include"
-IncludeDir["glfw"] = "%{wks.location}/EppoCore/EppoCore/Vendor/glfw/include"
-IncludeDir["glm"] = "%{wks.location}/EppoCore/EppoCore/Vendor/glm"
-IncludeDir["imgui"] = "%{wks.location}/EppoCore/EppoCore/Vendor/imgui"
-IncludeDir["spdlog"] = "%{wks.location}/EppoCore/EppoCore/Vendor/spdlog/include"
-IncludeDir["stb"] = "%{wks.location}/EppoCore/EppoCore/Vendor/stb"
